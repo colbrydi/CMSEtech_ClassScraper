783315482dcacb6986f4e722c08d3ae6114fa8cb
MacOS <macos@MacOSs-MacBook-Air-2.local>
2024-08-15 22:40:11 -0400

    Change waiting time

--- Login.py
+++ Login.py
@@ -10,7 +10,7 @@ def login_to_SIS(driver):
     
     element = driver.find_element(By.ID, 'loginUrl1') # Hit the login button
     element.click() 
-    time.sleep(5)
+    time.sleep(2)
 
     element = driver.find_element(By.ID, 'input28') # Find the MSU email input
     if not element.get_attribute('value'):
@@ -25,17 +25,17 @@ def login_to_SIS(driver):
     
     element = driver.find_element(By.CLASS_NAME, 'o-form-button-bar') # Click Sign in
     element.click()
-    time.sleep(7)
+    time.sleep(5)
     
     buttons = driver.find_elements(By.CLASS_NAME, 'authenticator-button')
     for button in buttons:
         if button.get_attribute('data-se') == 'phone_number':
             button.click() # Click on authenticate by phone
-    time.sleep(5)
+    time.sleep(3)
     
     element = driver.find_element(By.TAG_NAME, 'input')  
     element.click() # Click on Receive code via SMS
-    time.sleep(5)
+    time.sleep(3)
     
     element = driver.find_element(By.TAG_NAME, "input") # Find the code input
     print("Please enter your SMS code: ")
@@ -44,4 +44,4 @@ def login_to_SIS(driver):
      
     element = driver.find_element(By.CLASS_NAME, 'o-form-button-bar') # Click Verify
     element.click()
-    time.sleep(5)
+    time.sleep(3)
